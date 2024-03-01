cea40dd22cc40ba42dae3f5b5974f4842445ca76
sandro_mazzolla <jmazzolla94@gmail.com>
2024-03-01 19:01:02 -0300

    fixing import dependencies and adding login route/method

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from models.user import User
+from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+
+@app.route("/login", methods=["POST"])
+def login():
+    request_json = request.json
+    username = request_json.get("username")
+    password = request_json.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username = username).first()
+
+        if user and user.password == password:
+                return jsonify({"message": "AUTENTICAÇÃO REALIZADA COM SUCESSO"})
+
+    return jsonify({"message": "CREDÊNCIAS INVÁLIDAS"}), 400
 
 
 @app.route("/hello-world", methods=["GET"])
