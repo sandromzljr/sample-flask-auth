75f843c637f647fcee8cae59ca2329536ff14195
sandro_mazzolla <jmazzolla94@gmail.com>
2024-03-05 18:43:40 -0300

    FEAT: create role column and increment total size of password, so it can handle with cript

--- models/user.py
+++ models/user.py
@@ -4,4 +4,5 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(40), nullable=False, default="user")
